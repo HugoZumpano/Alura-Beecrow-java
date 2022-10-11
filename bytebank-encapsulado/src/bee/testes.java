0122cd46b511a8b4ab04836198bdb23273294aef
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-10-10 22:29:02 -0300

    atualizando estudandos

--- bytebank-encapsulado/src/bee/testes.java
+++ bytebank-encapsulado/src/bee/testes.java
@@ -1,4 +1,5 @@
 package bee;
+
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,23 +9,23 @@ import java.util.Scanner;
 public class testes {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		int valorA, valorB, valorC, valorD;
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		valorA = scan.nextInt();
 		valorB = scan.nextInt();
 		valorC = scan.nextInt();
 		valorD = scan.nextInt();
-		
-		if (valorB > valorC && valorD > valorA && valorC > 0 && valorD > 0 && (valorC + valorD) > 
-		(valorA + valorB) && valorA % 2 == 0) {
+
+		if (valorB > valorC && valorD > valorA && valorC > 0 && valorD > 0 && (valorC + valorD) > (valorA + valorB)
+				&& valorA % 2 == 0) {
 			System.out.println("Valores aceitos");
-			
-		}else {
-			System.out.println("Valores nao aceitos");
-			
+
+		} else {
+			System.out.println("Valores nao aceitoss");
+
 		}
 
 	}
